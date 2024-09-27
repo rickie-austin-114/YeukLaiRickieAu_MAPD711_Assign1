2e1ae3610ecb6e45b0b8d775268094f5f611193d
RickieAustin114 <rcya996001@gmail.com>
2024-09-27 11:08:12 +0800

    edited

--- app/src/main/java/com/example/yeuklairickieau_mapd711_assign1/MainActivity.kt
+++ app/src/main/java/com/example/yeuklairickieau_mapd711_assign1/MainActivity.kt
@@ -33,39 +33,38 @@ class MainActivity : AppCompatActivity() {
 
     //defining a button click event
     fun Button_Click(v: View) {
-        //checking whether the button 1 is clicked or not
-        if (v.id == R.id.button1) {
+        //checking whether the submit button is clicked or not
+        if (v.id == R.id.submit) {
             //creating an intent object using both first and second activities.
             val i = Intent(this@MainActivity, SecondActivity::class.java)
 
-            //java reference object for edit text control
+            // get the name input, send it to second activity
             val nameInput = findViewById<EditText>(R.id.nameInput)
-
-            //getting value from edit text and assign to string message
             val nameMessage = nameInput.text.toString()
-
-            //assigning value into intent object using key and values pairs.
             i.putExtra("Name", nameMessage)
 
+            // get the address input, send it to second activity
             val addressInput = findViewById<EditText>(R.id.addressInput)
             val addressMessage = addressInput.text.toString()
             i.putExtra("Address", addressMessage)
 
+            // get the profession input, send it to second activity
             val professionInput = findViewById<EditText>(R.id.professionInput)
             val professionMessage = professionInput.text.toString()
             i.putExtra("Profession", professionMessage)
 
+            // get the dream job input, send it to second activity
             val dreamJobInput = findViewById<EditText>(R.id.dreamJobInput)
             val dreamJobMessage = dreamJobInput.text.toString()
             i.putExtra("DreamJob", dreamJobMessage)
 
+            // get the favorite food input, send it to second activity
             val favoriteFoodInput = findViewById<EditText>(R.id.favoriteFoodInput)
             val favoriteFoodMessage = favoriteFoodInput.text.toString()
             i.putExtra("FavoriteFood", favoriteFoodMessage)
 
-            //starting sub activity
+            //starting second activity
             startActivity(i)
-
         }
     }
 }
--- app/src/main/java/com/example/yeuklairickieau_mapd711_assign1/RickieFragment.kt
+++ app/src/main/java/com/example/yeuklairickieau_mapd711_assign1/RickieFragment.kt
@@ -7,12 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.appcompat.app.AppCompatActivity
 
 
-
-
-
+// this file is for task 2
 class RickieFragment : Fragment()  {
 
     // initiate the 4 labels displaying the text
@@ -25,27 +22,35 @@ class RickieFragment : Fragment()  {
 
         val view = inflater.inflate(R.layout.fragment_rickie, container, false)
 
-        //
+        // connect the 4 TextView that display the text
         onCreateViewText = view.findViewById<TextView>(R.id.onCreateViewLabel)
         onStartText = view.findViewById<TextView>(R.id.onStartLabel)
         onResumeText = view.findViewById<TextView>(R.id.onResumeLabel)
         onPauseText = view.findViewById<TextView>(R.id.onPauseLabel)
+
+        // display the message after onCreateView() is invoked
         onCreateViewText.text = "onCreateView() invoked"
 
         return view
     }
 
+    // display the message after onStart() is invoked
     override fun onStart() {
+        // inherit the onStart() function, prevent it from being modified
         super.onStart()
         onStartText.text = "OnStart() invoked"
     }
 
+    // display the message after onResume() is invoked
     override fun onResume() {
+        // inherit the onResume() function, prevent it from being modified
         super.onResume()
         onResumeText.text = "onResume() invoked"
     }
 
+    // display the message after onPause() is invoked
     override fun onPause() {
+        // inherit the onPause() function, prevent it from being modified
         super.onPause()
         onPauseText.text = "onPause() invoked"
     }
